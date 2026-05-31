f02a412f145287ce58536eb4dc6a8cd9ece25326
Luigi Gianellini <luigigianellini@gmail.com>
2026-05-31 14:25:43 -0300

    Primeira versão

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import funcoes
+
+app = Flask(__name__)
+historico = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    resultado = None
+
+    if request.method == "POST":
+        acao = request.form.get("acao")
+
+        if acao == "media":
+            np1 = float(request.form["np1"])
+            np2 = float(request.form["np2"])
+            pim = float(request.form["pim"])
+
+            resultado = funcoes.media(np1, np2, pim)
+            historico.append({
+    "tipo": "Média",
+    "NP1": np1,
+    "NP2": np2,
+    "PIM": pim,
+    "Resultado": resultado
+})
+
+        elif acao == "pim":
+            np1 = float(request.form["np1"])
+            np2 = float(request.form["np2"])
+
+            resultado = funcoes.especular(np1, np2)
+            historico.append({
+    "tipo": "Especulação PIM",
+    "NP1": np1,
+    "NP2": np2,
+    "Resultado": resultado
+})
+
+        elif acao == "exame":
+            media = float(request.form["media"])
+            exame = float(request.form["exame"])
+
+            resultado = funcoes.exame(media, exame)
+            historico.append({
+    "tipo": "Exame",
+    "Média": media,
+    "Exame": exame,
+    "Resultado": resultado
+})
+            
+
+    return render_template("index.html", resultado=resultado, historico=historico)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ funcoes.py
@@ -0,0 +1,82 @@
+
+c = {
+    "limpa": "\033[m",
+    "erro": "\033[1;31m", #vermelho
+    "sucesso": "\033[1;32m", #verde
+    "alerta": "\033[1;33m", #amarelo
+    "info": "\033[1;34m", #lilas
+    "roxo": "\033[1;35m" #roxo mais pra rosa =/
+}
+def leiaInt(msg):
+    try:
+        return int(input(msg))
+    except (ValueError, TypeError):
+        print(f"{c["erro"]}Digite apenas numeros inteiros!{c["limpa"]}")
+        raise ValueError 
+
+def leiaFloat(msg):
+    while True:
+        try:
+            pergunta = input(msg).replace(",", ".").strip()
+            numero = float(pergunta)
+            if numero > 10 or numero < 0:
+                print(f"{c["erro"]}Digite notas entre 0 e 10!{c["limpa"]}")
+            else:
+                return numero
+        except (ValueError, TypeError):
+            print(f"{c["erro"]}Digite apenas numeros inteiros!{c["limpa"]}")
+            continue
+    
+def linha(tam = 50):
+    print(f"{"-" * tam}")
+
+def cabecalho(msg, tam=50):
+    linha()
+    print(f"{msg}".center(tam))
+    linha()
+def menu(lista):
+    while True:
+        try:
+            cabecalho("MENU PRINCIPAL")
+            contador = 1
+            for item in lista:
+                print(f"{c["alerta"]}{contador}{c["limpa"]} -  {c["roxo"]}{item}{c["limpa"]}")
+                contador += 1
+            opcao = leiaInt("Sua opcao: ")
+            return opcao
+        except:
+            continue
+def media(np1, np2, pim=0):
+    try:
+        resultado = ((np1 + np2)/2) * 0.8 + pim * 0.2
+        if 6.68 <= resultado and resultado <= 7:
+            resultado =  7
+        return resultado
+    except (ValueError, TypeError):
+        print(f"{c['erro']} ERRO ao fazer o calculo {c['limpa']}")
+    except KeyboardInterrupt:
+        print(f"{c['erro']} ERRO acao cancelada pelo usuario {c['limpa']}")
+
+def especular(np1, np2):
+    try:
+        calculo = ((np1 + np2)/2) * 0.8
+        diferenca = 7 - calculo
+        if diferenca <= 0:
+            return f"{c['sucesso']}Voce ja passou, media {media(np1,np2)} (sem precisar do PIM) {c['limpa']}"
+        else:
+            valor_pim = round(diferenca / 0.2, 2)
+            if 6.68 <= valor_pim and valor_pim < 7:
+                valor_pim = 7
+            else:
+                if valor_pim > 10:
+                    return "Precisa de mais de 10 no PIM mano =/"
+                else:
+                    return valor_pim
+    except:
+        return f"{c['erro']}ERRO ao especular {c['limpa']}"
+
+def exame(media=0, exame=0):
+    try:
+        return (media + exame)/2
+    except:
+        return f"{c['erro']}ERRO ao calcular exame {c['limpa']}"
